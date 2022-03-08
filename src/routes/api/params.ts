ceb4d260d31b013bb626b5f4acff9c1e9df3f76c
Darby Manning <darby@kyan.com>
2022-03-08 19:41:55 +0000

    test

---
+++ src/routes/api/params.ts
@@ -0,0 +1,55 @@
+import { readFileSync, existsSync, mkdirSync } from "fs";
+import { resolve } from "path";
+import type { RequestHandler } from "@sveltejs/kit";
+import sharp, { type FormatEnum } from "sharp";
+
+async function optimiseAndSaveImage(filename: string, format: keyof FormatEnum, outputFull: string, width?: number) {
+  const buffer = readFileSync(resolve("static", filename));
+  await sharp(buffer)
+    .resize(width || null)
+    .toFormat(format)
+    .toFile(outputFull);
+}
+
+function getOutputPath(filename: string, format: keyof FormatEnum, outputFolder: string, width?: number) {
+  let name = filename.split(".").slice(0, -1).join(".");
+  if (width) name += `__${width}`;
+  return `${outputFolder}/${name}.${format}`;
+}
+
+export const get: RequestHandler = async (payload) => {
+  return {
+    body: {
+      searchParamsString: payload.url.searchParams.toString(),
+    },
+  };
+
+  const { url } = payload;
+
+  const [filename, format, width] = [
+    url.searchParams.get("file"),
+    url.searchParams.get("format") as unknown as keyof FormatEnum,
+    Number(url.searchParams.get("width")),
+  ];
+
+  if (!filename) {
+    return {
+      body: "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+    };
+  }
+
+  const outputFolder = resolve(`.svelte-kit/output/client/_app/assets/cache/images/${filename}`);
+  if (!existsSync(outputFolder)) {
+    mkdirSync(outputFolder, { recursive: true });
+  }
+
+  const outputPath = getOutputPath(filename, format, outputFolder, width);
+
+  if (!existsSync(outputPath)) {
+    await optimiseAndSaveImage(filename, format, outputPath, width);
+  }
+
+  return {
+    body: readFileSync(outputPath),
+  };
+};
