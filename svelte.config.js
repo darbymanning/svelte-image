6ba53484fb6a609049a3bc83004b2be8b6930f22
Darby Manning <darby@kyan.com>
2022-03-09 21:31:25 +0000

    Try using new `external` arg in adapter

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({ external: ["sharp"] }),
 
     // Override http methods in the Todo forms
     methodOverride: {
